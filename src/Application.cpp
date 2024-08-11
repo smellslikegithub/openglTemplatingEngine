5604d920575f554eb20636f6c9204b4a36dd5831
Ashish Husain <ashish.m.husain@gmail.com>
2024-08-11 18:12:09 +0200

    feat(render_engine): Setting up GLEW and GLFW.
    Additional Information:
    => Setting up the dependencies for static GLEW and GLFW for crossplatform windowing and interfacing
    with opengl driver code.
    
    => Checking if the opengl works using by drawing a traingle using old opengl standard

---
+++ src/Application.cpp
@@ -0,0 +1,60 @@
+#include<GL/glew.h> // make sure this is the first include
+#include <GLFW/glfw3.h>
+#include<stdio.h>
+
+
+// prototypes
+void checkOpenGlWorks();
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    
+
+    
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        checkOpenGlWorks();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void checkOpenGlWorks() {
+    
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+}
